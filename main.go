6cb946a339912d2b9b5a1d2fb113ea4bd8ae9dc3
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-02-25 15:19:15 +0200

    Run step in working_dir provided

--- main.go
+++ main.go
@@ -6,38 +6,68 @@ import (
 	"os/exec"
 
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-tools/go-steputils/stepconf"
 )
 
+// Config ...
+type Config struct {
+	WorkDir string `env:"work_dir,dir"`
+}
+
+func initializeConfig() (Config, error) {
+	var config Config
+	if err := stepconf.Parse(&config); err != nil {
+		fmt.Printf("Error parsing step config: %s\n", err)
+		os.Exit(1)
+	}
+	stepconf.Print(config)
+
+	workDir, err := pathutil.AbsPath(config.WorkDir)
+	if err != nil {
+		fmt.Printf("Error normalizing workdir path: %s", err)
+	}
+
+	exists, err := pathutil.IsDirExists(workDir)
+	if err != nil {
+		fmt.Printf("Error validating workdir `%s`: %s", workDir, err)
+	}
+	if !exists {
+		fmt.Printf("Specified path `%s` does not exist", workDir)
+	}
+	return config, nil
+}
+
 func main() {
+	config, _ := initializeConfig()
+
 	fmt.Println()
 	log.Infof("Building Jekyll project")
 
-	cmdBundleInstallResult, err := exec.Command("bundle", "install").CombinedOutput()
+	cmdBundleInstall := exec.Command("bundle", "install")
+	cmdBundleInstall.Dir = config.WorkDir
+	cmdBundleInstallResult, err := cmdBundleInstall.Output()
 	if err != nil {
 		fmt.Printf("Failed to install dependencies using bundle, error: %#v | output: %s", err, cmdBundleInstallResult)
 		os.Exit(1)
 	}
 	fmt.Printf(string(cmdBundleInstallResult))
 
-	cmdJekyllBuildResult, err := exec.Command("bundle", "exec", "jekyll", "build").CombinedOutput()
+	cmdJekyllBuild := exec.Command("bundle", "exec", "jekyll", "build")
+	cmdJekyllBuild.Dir = config.WorkDir
+	cmdJekyllBuildResult, err := cmdJekyllBuild.Output()
 	if err != nil {
 		fmt.Printf("Failed to build Jekyll project, error: %#v | output: %s", err, cmdJekyllBuildResult)
 		os.Exit(1)
 	}
 	fmt.Printf(string(cmdJekyllBuildResult))
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "JEKYLL_GENERATED_SITE_FOLDER", "--value", "the value you want to share").CombinedOutput()
+	siteDir := config.WorkDir + "/_site"
+	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "JEKYLL_GENERATED_SITE_FOLDER", "--value", siteDir).CombinedOutput()
 	if err != nil {
 		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
 		os.Exit(1)
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
 
 	log.Donef("  Done")
 }
