101115e620d90213b56e8098b87560d5696e9d8d
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-02-25 14:15:21 +0200

    Execute Jekyll commands from default working directory

--- main.go
+++ main.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/bitrise-io/go-utils/log"
 )
 
 func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+	fmt.Println()
+	log.Infof("Building Jekyll project")
+
+	cmdBundleInstallResult, err := exec.Command("bundle", "install").CombinedOutput()
+	if err != nil {
+		fmt.Printf("Failed to install dependencies using bundle, error: %#v | output: %s", err, cmdBundleInstallResult)
+		os.Exit(1)
+	}
+	fmt.Printf(string(cmdBundleInstallResult))
+
+	cmdJekyllBuildResult, err := exec.Command("bundle", "exec", "jekyll", "build").CombinedOutput()
+	if err != nil {
+		fmt.Printf("Failed to build Jekyll project, error: %#v | output: %s", err, cmdJekyllBuildResult)
+		os.Exit(1)
+	}
+	fmt.Printf(string(cmdJekyllBuildResult))
 
 	//
 	// --- Step Outputs: Export Environment Variables for other Steps:
 	// You can export Environment Variables for other Steps with
 	//  envman, which is automatically installed by `bitrise setup`.
 	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "JEKYLL_GENERATED_SITE_FOLDER", "--value", "the value you want to share").CombinedOutput()
 	if err != nil {
 		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
 		os.Exit(1)
@@ -22,10 +39,5 @@ func main() {
 	// You can find more usage examples on envman's GitHub page
 	//  at: https://github.com/bitrise-io/envman
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
-	os.Exit(0)
+	log.Donef("  Done")
 }
